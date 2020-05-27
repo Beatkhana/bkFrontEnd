8eb48eb14bc753640840cd2060fe4b215798baa9
Dan Carruthers <dan@pop-os.localdomain>
2020-05-27 18:19:30 +1000

    yes

--- server/router.ts
+++
@@ -1,9 +0,0 @@
-var routerExpress = require('express');
-var router = routerExpress.Router();
-
-const baseUrl = '/api';
-
-// Basic routes
-router.get(baseUrl, (req, res) => res.send({hello: 'there'}));
-
-module.exports = router;

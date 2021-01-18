4da5ef10dd7113515d9546e3dcac6f91fd42c0e6
Dannypoke03 <dannypoke03@beatsaberleague.com>
2021-01-18 23:20:00 +1100

    add sign up error messages

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -3,6 +3,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AppComponent } from '../app.component';
+import { MatDialogRef } from '@angular/material/dialog';
+import { editProfileDialog } from '../profile/profile.component';
+import { NotificationService } from '../services/toast.service';
 
 @Component({
     selector: 'app-sign-up',
@@ -21,7 +25,10 @@ export class SignUpComponent implements OnInit {
     constructor(
         private fb: FormBuilder,
         public http: HttpClient,
-        private router: Router) { }
+        private notif: NotificationService
+    ) { }
+
+    
 
     ngOnInit(): void {
         this.signUpForm = this.fb.group({
@@ -38,12 +45,17 @@ export class SignUpComponent implements OnInit {
         });
     }
 
-    onSubmit() {
-        this.submitData()
-            .subscribe(data => {
-                console.log(data);
-                window.location.href = '/';
-            });
+    async onSubmit() {
+        try {
+            await this.http.post(this.url, this.signUpForm.value).toPromise();
+            this.notif.showSuccess('', 'Successfully created profile');
+            window.location.href = '/';
+        } catch (error) {
+            console.error("Error: ", error);
+            this.notif.showError('', 'Error creating profile');
+            
+            if (error.error?.message?.includes('ER_DUP_ENTRY')) this.notif.showError('', 'Discord or Scoresaber account already in use');
+        }
     }
 
     submitData(): Observable<any> {

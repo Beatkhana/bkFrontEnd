3151d85fa22c6ca7b3d8deb398531d46a9cf9198
Dannypoke03 <dannypoke03@beatsaberleague.com>
2021-04-08 14:44:47 +1000

    fix false ta connection reporting + live quals no fail compensation

--- src/app/qualifiers/qualifiers.component.ts
+++ src/app/qualifiers/qualifiers.component.ts
@@ -4,139 +4,145 @@ import { Observable } from 'rxjs';
 import { addSongDialog } from '../map-pool/map-pool.component';
 
 @Component({
-	selector: 'app-qualifiers',
-	templateUrl: './qualifiers.component.html',
-	styleUrls: ['./qualifiers.component.scss']
+    selector: 'app-qualifiers',
+    templateUrl: './qualifiers.component.html',
+    styleUrls: ['./qualifiers.component.scss']
 })
 export class QualifiersComponent implements OnInit {
 
-	@Input() tournament;
+    @Input() tournament;
 
-	qualsScores = [];
-	loading = true;
+    qualsScores = [];
+    loading = true;
 
-	pools: any = [];
-	leaderboards = {};
-	qualsPool;
+    pools: any = [];
+    leaderboards = {};
+    qualsPool;
 
 
-	constructor(public http: HttpClient) { }
+    constructor(public http: HttpClient) { }
 
-	async ngOnInit(): Promise<void> {
-		let pools = await this.http.get(`api/tournament/${this.tournament.tournamentId}/map-pools`).toPromise();
-		this.qualsPool = Object.values(pools).find(x => x.is_qualifiers == 1);
+    async ngOnInit(): Promise<void> {
+        let pools = await this.http.get(`api/tournament/${this.tournament.tournamentId}/map-pools`).toPromise();
+        let tmp = Object.values(pools).filter(x => x.is_qualifiers == 1);
+        this.qualsPool = Object.values(pools).find(x => x.is_qualifiers == 1);
+        for (let pool of tmp) {
+            if (pool.id != this.qualsPool.id) {
+                this.qualsPool.songs = [...this.qualsPool.songs, ...pool.songs];
+            }
+        }
 
-		this.getQuals()
-			.subscribe(res => {
-				this.qualsScores = res;
-				for (const user of this.qualsScores) {
-					if (user.avatar.includes('api') || user.avatar.includes('oculus')) {
-						user.avatar = "https://new.scoresaber.com" + user.avatar;
-					} else {
-						user.avatar = `/${user.avatar}` + (user.avatar.substring(0, 2) == 'a_' ? '.gif' : '.webp');
-						user.avatar = `https://cdn.discordapp.com/avatars/${user.discordId}${user.avatar}`
-					}
-					for (const score of user.scores) {
-						if (this.qualsPool.songs.find(x => x.hash == score.songHash)?.numNotes != 0) {
-							score.percentage = score.score / (this.qualsPool.songs.find(x => x.hash == score.songHash)?.numNotes * 920 - 7245)
-						} else {
-							score.percentage = 0;
-						}
-						// score.score = Math.round(score.score / 2);
-						if (score.songHash in this.leaderboards) {
-							this.leaderboards[score.songHash].push({
-								discordId: user.discordId,
-								name: user.name,
-								avatar: user.avatar,
-								score: score.score,
-								percentage: (score.percentage * 100).toFixed(2)
-							});
-						} else {
-							this.leaderboards[score.songHash] = [{
-								discordId: user.discordId,
-								name: user.name,
-								avatar: user.avatar,
-								score: score.score,
-								percentage: (score.percentage * 100).toFixed(2)
-							}];
-						}
-					}
+        this.getQuals()
+            .subscribe(res => {
+                this.qualsScores = res;
+                for (const user of this.qualsScores) {
+                    if (user.avatar.includes('api') || user.avatar.includes('oculus')) {
+                        user.avatar = "https://new.scoresaber.com" + user.avatar;
+                    } else {
+                        user.avatar = `/${user.avatar}` + (user.avatar.substring(0, 2) == 'a_' ? '.gif' : '.webp');
+                        user.avatar = `https://cdn.discordapp.com/avatars/${user.discordId}${user.avatar}`
+                    }
+                    for (const score of user.scores) {
+                        if (this.qualsPool.songs.find(x => x.hash == score.songHash)?.numNotes != 0) {
+                            score.percentage = score.score / (this.qualsPool.songs.find(x => x.hash == score.songHash)?.numNotes * 920 - 7245)
+                        } else {
+                            score.percentage = 0;
+                        }
+                        // score.score = Math.round(score.score / 2);
+                        if (score.songHash in this.leaderboards) {
+                            this.leaderboards[score.songHash].push({
+                                discordId: user.discordId,
+                                name: user.name,
+                                avatar: user.avatar,
+                                score: score.score,
+                                percentage: (score.percentage * 100).toFixed(2)
+                            });
+                        } else {
+                            this.leaderboards[score.songHash] = [{
+                                discordId: user.discordId,
+                                name: user.name,
+                                avatar: user.avatar,
+                                score: score.score,
+                                percentage: (score.percentage * 100).toFixed(2)
+                            }];
+                        }
+                    }
 
-				}
-				// leadboards.sort((a,b) => a.score)
-				for (const leaderboard of Object.keys(this.leaderboards)) {
-					this.leaderboards[leaderboard].sort((a, b) => b.score - a.score);
-				}
-				for (const user of this.qualsScores) {
-					for (const score of user.scores) {
-						score.position = this.leaderboards[score.songHash].findIndex(x => x.discordId == user.discordId);
-					}
-					user.scores.sort((a, b) => (a.songHash > b.songHash) ? 1 : ((a.songHash < b.songHash) ? -1 : 0));
-				}
+                }
+                // leadboards.sort((a,b) => a.score)
+                for (const leaderboard of Object.keys(this.leaderboards)) {
+                    this.leaderboards[leaderboard].sort((a, b) => b.score - a.score);
+                }
+                for (const user of this.qualsScores) {
+                    for (const score of user.scores) {
+                        score.position = this.leaderboards[score.songHash].findIndex(x => x.discordId == user.discordId);
+                    }
+                    user.scores.sort((a, b) => (a.songHash > b.songHash) ? 1 : ((a.songHash < b.songHash) ? -1 : 0));
+                }
 
-				if (this.qualsScores.length == 1) {
-					let sumA = this.sumProperty(this.qualsScores[0].scores, 'score');
-					let sumAPer = this.sumProperty(this.qualsScores[0].scores, 'percentage');
-					this.qualsScores[0].avgPercentage = isNaN(sumAPer / this.qualsPool.songs.length * 100) ? 0 : (sumAPer / this.qualsPool.songs.length * 100).toFixed(2);
-					this.qualsScores[0].scoreSum = sumA;
-				}
+                if (this.qualsScores.length == 1) {
+                    let sumA = this.sumProperty(this.qualsScores[0].scores, 'score');
+                    let sumAPer = this.sumProperty(this.qualsScores[0].scores, 'percentage');
+                    this.qualsScores[0].avgPercentage = isNaN(sumAPer / this.qualsPool.songs.length * 100) ? 0 : (sumAPer / this.qualsPool.songs.length * 100).toFixed(2);
+                    this.qualsScores[0].scoreSum = sumA;
+                }
 
-				this.qualsScores.sort((a, b) => {
-					let sumA = this.sumProperty(a.scores, 'score');
-					let sumB = this.sumProperty(b.scores, 'score');
-					let sumAPer = this.sumProperty(a.scores, 'percentage');
-					let sumBPer = this.sumProperty(b.scores, 'percentage');
-					a.avgPercentage = isNaN(sumAPer / this.qualsPool.songs.length * 100) ? 0 : (sumAPer / this.qualsPool.songs.length * 100).toFixed(2);
-					b.avgPercentage = isNaN(sumBPer / this.qualsPool.songs.length * 100) ? 0 : (sumBPer / this.qualsPool.songs.length * 100).toFixed(2);
-					a.scoreSum = sumA;
-					b.scoreSum = sumB;
-					if (b.avgPercentage == a.avgPercentage) {
-						if (sumB == sumA) {
-							if (a.globalRank == 0) return 1;
-							if (b.globalRank == 0) return -1;
-							return a.globalRank - b.globalRank;
-						} else {
-							return sumB - sumA;
-						}
-					} else {
-						return b.avgPercentage - a.avgPercentage;
-					}
+                this.qualsScores.sort((a, b) => {
+                    let sumA = this.sumProperty(a.scores, 'score');
+                    let sumB = this.sumProperty(b.scores, 'score');
+                    let sumAPer = this.sumProperty(a.scores, 'percentage');
+                    let sumBPer = this.sumProperty(b.scores, 'percentage');
+                    a.avgPercentage = isNaN(sumAPer / this.qualsPool.songs.length * 100) ? 0 : (sumAPer / this.qualsPool.songs.length * 100).toFixed(2);
+                    b.avgPercentage = isNaN(sumBPer / this.qualsPool.songs.length * 100) ? 0 : (sumBPer / this.qualsPool.songs.length * 100).toFixed(2);
+                    a.scoreSum = sumA;
+                    b.scoreSum = sumB;
+                    if (b.avgPercentage == a.avgPercentage) {
+                        if (sumB == sumA) {
+                            if (a.globalRank == 0) return 1;
+                            if (b.globalRank == 0) return -1;
+                            return a.globalRank - b.globalRank;
+                        } else {
+                            return sumB - sumA;
+                        }
+                    } else {
+                        return b.avgPercentage - a.avgPercentage;
+                    }
 
-				});
+                });
 
-				this.qualsScores.splice(this.tournament.quals_cutoff, 0, {
-					cutoff: true
-				});
-				let i = 1;
-				for (const user of this.qualsScores) {
-					if (!user.cutoff) {
-						user.position = i;
-						i++;
-					}
-				}
-				this.loading = false;
-			})
-	}
+                this.qualsScores.splice(this.tournament.quals_cutoff, 0, {
+                    cutoff: true
+                });
+                let i = 1;
+                for (const user of this.qualsScores) {
+                    if (!user.cutoff) {
+                        user.position = i;
+                        i++;
+                    }
+                }
+                this.loading = false;
+            })
+    }
 
-	public getQuals(): Observable<any> {
-		return this.http.get(`/api/tournament/${this.tournament.tournamentId}/qualifiers`);
-	}
+    public getQuals(): Observable<any> {
+        return this.http.get(`/api/tournament/${this.tournament.tournamentId}/qualifiers`);
+    }
 
-	async getPools() {
-		return await this.http.get(`api/tournament/${this.tournament.tournamentId}/map-pools`).toPromise();
-	}
+    async getPools() {
+        return await this.http.get(`api/tournament/${this.tournament.tournamentId}/map-pools`).toPromise();
+    }
 
-	findSong(hash: string) {
-		return this.qualsPool.songs.find(x => x.hash == hash);
-	}
+    findSong(hash: string) {
+        return this.qualsPool.songs.find(x => x.hash == hash);
+    }
 
-	sumProperty(items, prop) {
-		if (items == null) {
-			return 0;
-		}
-		return items.reduce(function (a, b) {
-			return b[prop] == null ? a : a + b[prop];
-		}, 0);
-	}
+    sumProperty(items, prop) {
+        if (items == null) {
+            return 0;
+        }
+        return items.reduce(function (a, b) {
+            return b[prop] == null ? a : a + b[prop];
+        }, 0);
+    }
 
 }
